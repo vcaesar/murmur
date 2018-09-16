f005b68c42f0a064716a71a34194e2deb85ab800
vcaesar <vzvway@gmail.com>
2018-09-16 12:49:47 -0400

    update and simplify murmur test

--- murmur_test.go
+++ murmur_test.go
@@ -2,36 +2,30 @@ package murmur
 
 import (
 	"testing"
+
+	"github.com/vcaesar/tt"
 )
 
 func TestMurmur3(t *testing.T) {
 	text1 := []byte("The quick brown fox jumps over the lazy dog")
 	expectedHash1 := uint32(0x78e69e27)
 	actualHash1 := Murmur3(text1)
-	if expectedHash1 != actualHash1 {
-		t.Errorf("Expected hash value: %d, got %d", expectedHash1, actualHash1)
-	}
+	tt.Equal(t, expectedHash1, actualHash1)
 
 	text2 := []byte("The quick brown fox jumps over the lazy cog")
 	expectedHash2 := uint32(0xd5ece287)
 	actualHash2 := Murmur3(text2, 1)
-	if expectedHash2 != actualHash2 {
-		t.Errorf("Expected hash value: %d, got %d", expectedHash2, actualHash2)
-	}
+	tt.Equal(t, expectedHash2, actualHash2)
 }
 
 func TestSum32(t *testing.T) {
 	text1 := "The quick brown fox jumps over the lazy dog"
 	expectedHash1 := uint32(0x78e69e27)
 	actualHash1 := Sum32(text1)
-	if expectedHash1 != actualHash1 {
-		t.Errorf("Expected hash value: %d, got %d", expectedHash1, actualHash1)
-	}
+	tt.Equal(t, expectedHash1, actualHash1)
 
 	text2 := "The quick brown fox jumps over the lazy cog"
 	expectedHash2 := uint32(0xd5ece287)
 	actualHash2 := Sum32(text2, 1)
-	if expectedHash2 != actualHash2 {
-		t.Errorf("Expected hash value: %d, got %d", expectedHash2, actualHash2)
-	}
+	tt.Equal(t, expectedHash2, actualHash2)
 }
