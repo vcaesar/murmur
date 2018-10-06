19d52eac0fe2659e958a32b676bbb8abe5b82e31
vcaesar <vzvway@gmail.com>
2018-10-06 10:03:48 -0400

    add benchmark code and simplify test code

--- murmur_test.go
+++ murmur_test.go
@@ -6,25 +6,44 @@ import (
 	"github.com/vcaesar/tt"
 )
 
+var (
+	testText = "The quick brown fox jumps over the lazy dog"
+	text2    = "The quick brown fox jumps over the lazy cog"
+)
+
+func Benchmark_Sum32(b *testing.B) {
+	fn := func() {
+		Sum32(testText)
+	}
+
+	tt.BM(b, fn)
+
+}
+
+func Benchmark_Murmur3(b *testing.B) {
+	fn := func() {
+		Murmur3([]byte(testText))
+	}
+
+	tt.BM(b, fn)
+}
+
 func TestMurmur3(t *testing.T) {
-	text1 := []byte("The quick brown fox jumps over the lazy dog")
+	text1 := []byte(testText)
 	expectedHash1 := uint32(0x78e69e27)
 	actualHash1 := Murmur3(text1)
 	tt.Equal(t, expectedHash1, actualHash1)
 
-	text2 := []byte("The quick brown fox jumps over the lazy cog")
 	expectedHash2 := uint32(0xd5ece287)
-	actualHash2 := Murmur3(text2, 1)
+	actualHash2 := Murmur3([]byte(text2), 1)
 	tt.Equal(t, expectedHash2, actualHash2)
 }
 
 func TestSum32(t *testing.T) {
-	text1 := "The quick brown fox jumps over the lazy dog"
 	expectedHash1 := uint32(0x78e69e27)
-	actualHash1 := Sum32(text1)
+	actualHash1 := Sum32(testText)
 	tt.Equal(t, expectedHash1, actualHash1)
 
-	text2 := "The quick brown fox jumps over the lazy cog"
 	expectedHash2 := uint32(0xd5ece287)
 	actualHash2 := Sum32(text2, 1)
 	tt.Equal(t, expectedHash2, actualHash2)
