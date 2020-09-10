f2a623226711b8cca4b22004028e283022a4ae04
vcaesar <vzvway@gmail.com>
2020-09-10 12:38:56 -0400

    optimize code and update circle.yml

--- murmur.go
+++ murmur.go
@@ -55,10 +55,10 @@ func Sum32(key string, seed ...uint32) (hash uint32) {
 		hash = seed[0]
 	}
 
-	iByte := 0
-	for ; iByte+4 <= len(key); iByte += 4 {
-		k := uint32(key[iByte]) | uint32(key[iByte+1])<<8 |
-			uint32(key[iByte+2])<<16 | uint32(key[iByte+3])<<24
+	var nbytes = len(key) / 4 * 4
+	for i := 0; i < nbytes; i += 4 {
+		k := uint32(key[i]) | uint32(key[i+1])<<8 |
+			uint32(key[i+2])<<16 | uint32(key[i+3])<<24
 
 		k *= c1
 		k = (k << 15) | (k >> 17)
@@ -70,15 +70,15 @@ func Sum32(key string, seed ...uint32) (hash uint32) {
 	}
 
 	var remaining uint32
-	switch len(key) - iByte {
+	switch len(key) & 3 {
 	case 3:
-		remaining += uint32(key[iByte+2]) << 16
+		remaining += uint32(key[nbytes+2]) << 16
 		fallthrough
 	case 2:
-		remaining += uint32(key[iByte+1]) << 8
+		remaining += uint32(key[nbytes+1]) << 8
 		fallthrough
 	case 1:
-		remaining += uint32(key[iByte])
+		remaining += uint32(key[nbytes])
 		remaining *= c1
 
 		remaining = (remaining << 15) | (remaining >> 17)
